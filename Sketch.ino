b26e50eb8820fcd8cb0926d95d9ab78e1ffc3858
drwalj <77970196+drwalj@users.noreply.github.com>
2022-01-18 09:19:11 +0100

    Add files via upload
    Code vom 11.01.2022, 
    es fehlt noch eine Überarbeitung des Linken knopfs, beim draufklicken soll nicht direkt kaffe kommen, sondern noch ein weiteres fenster wo die current temperatur und time angezeigt werden, dann soll ausgewählt werden können ob mit dem rechten knopf kaffe kommt oder man doch wieder in den startbildschirm mit dem linken knopf zurückkehr
    
    ++ Überarbeitung Sanduhrfunktion, zeit soll nicht um 5 geändert sondern um 2 oder 1 werden, und es soll keine zeit höher als 30 und weniger als 5 möglich sein.

---
+++ Sketch.ino
@@ -0,0 +1,394 @@
+
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+// Bitmaps:
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+
+
+const unsigned char bitmap_arrow[] PROGMEM = {
+  B00011000,
+  B00111000,
+  B00111000,
+  B00111000,
+  B00111000,
+  B00111000,
+  B00111000,
+  B00111000,
+  B10111001,
+  B11111011,
+  B11111111,
+  B11111111,
+  B11111111,
+  B01111110,
+  B00111100,
+  B00000000
+};
+
+/**
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
+ *
+ * This bitmap from the file 'wanduhr (2).png'
+ */
+#pragma once
+
+#define GRQ7BI_BMPWIDTH  32
+
+const unsigned char bitmap_grq7bi[] PROGMEM = {
+  B00000000,B00011111,B11111000,B00000000,
+  B00000000,B01111111,B11111110,B00000000,
+  B00000001,B11111111,B11111111,B10000000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000111,B11111100,B00111111,B11100000,
+  B00001111,B11100000,B00000111,B11110000,
+  B00011111,B10000000,B00000001,B11111000,
+  B00111111,B00000000,B00000000,B11111100,
+  B00111110,B00000001,B10000000,B01111100,
+  B01111100,B00000001,B10000000,B00111110,
+  B01111100,B00000001,B10000000,B00111110,
+  B11111000,B00000001,B10000000,B00011111,
+  B11111000,B00000001,B10000000,B00011111,
+  B11111000,B00000001,B10000000,B00011111,
+  B11110000,B00000001,B10000000,B00001111,
+  B11110000,B00000001,B11111111,B10001111,
+  B11110000,B00000001,B11111111,B10001111,
+  B11110000,B00000000,B00000000,B00001111,
+  B11111000,B00000000,B00000000,B00011111,
+  B11111000,B00000000,B00000000,B00011111,
+  B11111000,B00000000,B00000000,B00011111,
+  B01111100,B00000000,B00000000,B00111110,
+  B01111100,B00000000,B00000000,B00111110,
+  B00111110,B00000000,B00000000,B01111100,
+  B00111111,B00000000,B00000000,B11111100,
+  B00011111,B10000000,B00000001,B11111000,
+  B00001111,B11100000,B00000111,B11110000,
+  B00000111,B11111100,B00111111,B11100000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000001,B11111111,B11111111,B10000000,
+  B00000000,B01111111,B11111110,B00000000,
+  B00000000,B00011111,B11111000,B00000000
+};
+
+
+
+#define VFIKIF_BMPWIDTH  48
+
+const unsigned char bitmap_test[] PROGMEM = {
+  B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00010000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00010000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00110000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00010000,B10000000,B00000000,B00000000,
+  B00000000,B00000001,B00011000,B10000000,B00000000,B00000000,
+  B00000000,B00000001,B00011100,B10000000,B00000000,B00000000,
+  B00000000,B00000001,B10000110,B11000000,B00000000,B00000000,
+  B00000000,B00000000,B11000011,B01100000,B00000000,B00000000,
+  B00000000,B00000000,B01100011,B00110000,B00000000,B00000000,
+  B00000000,B00000000,B00100001,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00000001,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B00000000,B00000010,B00000000,B00000000,B00000000,
+  B00000000,B00001000,B00000000,B00000000,B01100000,B00000000,
+  B00000000,B00011111,B00000000,B00000011,B11111100,B00000000,
+  B00000000,B00001111,B11111111,B11111111,B11111110,B00000000,
+  B00000000,B00001111,B11111111,B11111111,B11000110,B00000000,
+  B00000000,B00001111,B11111111,B11111111,B11000111,B00000000,
+  B00000000,B00000111,B11111111,B11111111,B11000111,B00000000,
+  B00000000,B00000111,B11111111,B11111111,B10000111,B00000000,
+  B00000000,B00000111,B11111111,B11111111,B11001110,B00000000,
+  B00000000,B00011111,B11111111,B11111111,B11111100,B00000000,
+  B00000000,B11100001,B11111111,B11111110,B01111110,B00000000,
+  B00000001,B10000000,B11111111,B11111100,B00000001,B10000000,
+  B00000011,B00000000,B01111111,B11111000,B00000001,B10000000,
+  B00000011,B10000000,B00001111,B11000000,B00000011,B10000000,
+  B00000001,B11110000,B00000000,B00000000,B00011111,B00000000,
+  B00000000,B11111111,B11100000,B00001111,B11111110,B00000000,
+  B00000000,B00011111,B11111111,B11111111,B11110000,B00000000,
+  B00000000,B00000000,B11111111,B11111110,B00000000,B00000000
+};
+
+#define SVJMLK_BMPWIDTH  32
+
+const unsigned char bitmap_time[] PROGMEM = {
+  B00000011,B11111111,B11111111,B11000000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000001,B10000000,B00000001,B10000000,
+  B00000001,B10000000,B00000001,B10000000,
+  B00000001,B10000000,B00000001,B10000000,
+  B00000001,B10111111,B11111101,B10000000,
+  B00000000,B10100000,B00000101,B00000000,
+  B00000000,B10100000,B00000101,B00000000,
+  B00000000,B11010000,B00001011,B00000000,
+  B00000000,B01010000,B00001010,B00000000,
+  B00000000,B00001000,B00010110,B00000000,
+  B00000000,B00110111,B11101100,B00000000,
+  B00000000,B00011001,B10011000,B00000000,
+  B00000000,B00001100,B00110000,B00000000,
+  B00000000,B00000100,B00100000,B00000000,
+  B00000000,B00000100,B00100000,B00000000,
+  B00000000,B00001100,B00110000,B00000000,
+  B00000000,B00011000,B00011000,B00000000,
+  B00000000,B00110000,B00001100,B00000000,
+  B00000000,B01100000,B00000110,B00000000,
+  B00000000,B01000011,B11000010,B00000000,
+  B00000000,B11001100,B00110011,B00000000,
+  B00000000,B10010000,B00001001,B00000000,
+  B00000000,B10100000,B00000100,B00000000,
+  B00000001,B10100000,B00000101,B00000000,
+  B00000001,B10100000,B00000111,B10000000,
+  B00000001,B10111111,B11111101,B10000000,
+  B00000001,B10000000,B00000001,B10000000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000011,B11111111,B11111111,B11000000,
+  B00000011,B11111111,B11111111,B11000000
+};
+
+
+/**
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
+ *
+ * This bitmap from the file 'temppppppppppppppppp.png'
+ */
+
+#define GWIS9C_BMPWIDTH  24
+
+const unsigned char bitmap_temp[] PROGMEM = {
+  B00000000,B01111100,B00000000,
+  B00000000,B11100111,B00000000,
+  B00000000,B10011001,B00000000,
+  B00000001,B10111101,B00000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10100101,B10000000,
+  B00000001,B10110101,B10000000,
+  B00000011,B00100101,B10000000,
+  B00000010,B01100110,B11000000,
+  B00000110,B11000011,B01100000,
+  B00000101,B10000001,B00100000,
+  B00000101,B10000001,B10100000,
+  B00000101,B10000001,B10100000,
+  B00000101,B10000001,B00100000,
+  B00000110,B11000011,B01100000,
+  B00000010,B01111110,B01000000,
+  B00000011,B00111100,B11000000,
+  B00000001,B11000011,B10000000,
+  B00000000,B01111110,B00000000
+};
+
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+ float Celcius=0;
+ float Fahrenheit=0;
+ int flag=3;
+ int Brewingtime = 20;
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1
+Adafruit_SH1106 display(OLED_RESET);
+
+const int pressureInput = A0; //select the analog input pin for the pressure transducer
+const int pressureZero = 102.4; //analog reading of pressure transducer at 0psi
+const int pressureMax = 921.6; //analog reading of pressure transducer at 100psi
+const int pressuretransducermaxPSI = 300; //psi value of transducer being used
+const int sensorreadDelay = 100; //constant integer to set the sensor read delay in milliseconds
+float pressureValue = 0; //variable to store the value coming from the pressure transducer
+int tempnum=50;
+int drucknum=50;
+int disnum=50;
+
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sensors.begin();
+  display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(7, OUTPUT);
+  digitalWrite(7,LOW);
+  Start();
+}
+
+void Heat(){ //KNOPF RECHTS
+  while(true){
+    Druck();
+    if (digitalRead(2)==LOW){
+      delay(400);
+      break;
+    }
+    Temps();
+    if (digitalRead(2)==LOW){
+      delay(400);
+      break;
+    }
+    Display();
+    if (digitalRead(2)==LOW){
+      delay(400);
+      break;
+    }
+  }
+}
+
+void mayer(){ // KNOPF LINKS 
+
+    display.clearDisplay();
+    display.setTextSize(1,2);
+    display.setTextColor(WHITE);
+    display.setCursor(5,15);
+    display.print("Current Settings:")
+    
+
+    
+    display.clearDisplay();
+    display.setTextSize(1,2);
+    display.setTextColor(WHITE);
+    display.setCursor(5,15);
+    display.print("Brewing for "+ String(Brewingtime) + "s");
+    sensors.requestTemperatures();
+    Celcius=sensors.getTempCByIndex(0);
+    display.setCursor(5,40);
+    display.print("Brewing at "+  String(Celcius) + "C");
+    display.display();
+  
+    pinMode(7,OUTPUT);
+    digitalWrite(7,HIGH);
+    delay(500);
+    digitalWrite(7,LOW);
+    delay((Brewingtime*1000)-1000);
+    digitalWrite(7,HIGH);
+    delay(500);
+    digitalWrite(7,LOW);
+}
+
+void firstloop(){ 
+  while (true){
+    Start();
+    if (digitalRead(2)==LOW){
+      Sanduhr();
+    }
+    if (digitalRead(3)==LOW){
+      Heat();
+    }
+    if (digitalRead(4)==LOW){
+      mayer();
+    }
+  }
+}
+
+void loop() {
+ firstloop();
+}
+ 
+void Sanduhr(){
+  delay(600);
+  while (true){
+    display.clearDisplay();
+    display.setTextSize(1,2);
+    display.setTextColor(WHITE);
+    display.setCursor(57,35);
+    display.print(Brewingtime);
+    display.setCursor(69,35);
+    display.print("s");
+    display.drawBitmap(50,0,bitmap_grq7bi,32, 32, WHITE);
+    display.setCursor(20,50);
+    display.print("-");
+    display.setCursor(100,50);
+    display.print("+"); 
+    display.setCursor(65,50);
+    display.print("x");
+    display.display();
+
+    
+    if (digitalRead(2)==LOW){
+      delay(600);
+      break;
+    }
+    
+    if (digitalRead(3)==LOW){
+      delay(600);
+      Brewingtime += 5;
+    }
+    if (digitalRead(4)==LOW){
+      delay(600);
+      Brewingtime -= 5;
+    }
+  }
+}
+
+void Start(){
+  display.clearDisplay();
+  display.drawBitmap(0,10,bitmap_test,48 ,32,WHITE);
+  display.drawBitmap(20,50,bitmap_arrow,8 ,16,WHITE);
+  display.drawBitmap(60,50,bitmap_arrow,8 ,16,WHITE);
+  display.drawBitmap(100,50,bitmap_arrow,8 ,16,WHITE);
+  display.drawBitmap(50,10,bitmap_time,32 ,32,WHITE);
+  display.drawBitmap(90,10,bitmap_temp,24 ,32,WHITE);
+  display.display();
+}
+
+
+void Display(){
+  display.clearDisplay();
+  display.setTextSize(1,2);
+  display.setTextColor(WHITE);
+  display.setCursor(25,15);
+  
+  display.print(pressureValue, 1); //prints pressure value to lcd screen, 1 digit on float
+  
+  display.setCursor(50,15);
+  display.print("Bar");
+  display.setCursor(25,35);
+  display.print(Celcius);
+  display.setCursor(50,35);
+  display.print(" C");
+  display.display();
+}
+
+void Druck(){
+  pressureValue = analogRead(pressureInput); //reads value from input pin and assigns to variable
+  pressureValue = ((pressureValue-pressureZero)*pressuretransducermaxPSI)/(pressureMax-pressureZero); //conversion equation to convert analog reading to psi
+  pressureValue= pressureValue/14,5;
+}
+void Temps(){
+  sensors.requestTemperatures();
+  Celcius=sensors.getTempCByIndex(0);
+}
